27fff9ca9f4d762cdbd66d0b85216726e53277be
Ashish Ramesh <81244883+AshishRamesh@users.noreply.github.com>
2023-10-31 21:41:55 +0530

    Face Recognition using open CV

---
+++ Vid_Capture.py
@@ -0,0 +1,21 @@
+#Name:Ashish Ramesh
+#Github ID : AshishRamesh
+
+import cv2 as cv
+
+video = cv.VideoCapture(0)
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
+while True :
+    check,frame = video.read()
+    face = face_cascade.detectMultiScale(frame,
+    scaleFactor = 2,
+    minNeighbors = 5)
+    for x,y,w,h in face:
+        frame = cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+    cv.imshow('Video',frame)
+    key = cv.waitKey(1)
+    if key == ord('q'):
+        break
+
+video.release()
+cv.destroyAllWindows()
